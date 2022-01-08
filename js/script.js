884fff255486ef14a6c2155ca2e884dd030110d0
Anton Bekhtgold <jacepwncere@yandex.ru>
2022-01-08 08:16:55 +0300

    доработать аддаптив

---
+++ js/script.js
@@ -0,0 +1,37 @@
+new Swiper ('.mySlider', {
+    direction:'vertical',
+    autoplay: {
+      delay:5000
+    },
+    pagination : {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+			return '<span class="' + className + '"> 0' + (index + 1) + '</span>';
+		},
+    },
+    
+});
+//первый слайдер
+new Swiper ('.mySlider-2', {
+    loop: true,
+	autoplay: {
+		delay: 10000
+	 },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+// второй слайдер
+
+
+//бурег-меню
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+      $('.header__burger,.header__menu').toggleClass('active');
+    });
+});
